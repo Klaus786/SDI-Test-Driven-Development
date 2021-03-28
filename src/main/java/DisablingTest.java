bfca478c42ec2912c4c7e79e849d2f02af3550af
LTDO <lungatshila10@gmail.com>
2021-03-28 16:10:07 +0200

    made a few changes

--- src/main/java/DisablingTest.java
+++ src/main/java/DisablingTest.java
@@ -1,2 +1,14 @@
 public class DisablingTest {
+
+    //You see this class is empty...
+    //Lemme go to github and show you that its empty even there...
+
+    public DisablingTest(){
+
+    }
+
+    //NOW LETS PUSH THESE CHANGES TO GITHUB
+    public void printNames(){
+        System.out.println("Lunga Tshila");
+    }
 }
